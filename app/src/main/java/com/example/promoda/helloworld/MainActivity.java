dc27e787c9a29695a68481f6332a3f0ae5e099f1
Supun Pramoda Wanniarachchi <supunpramoda@gmail.com>
2016-04-03 15:08:10 +0530

    last commit end of day 1

--- app/src/main/java/com/example/promoda/helloworld/MainActivity.java
+++ app/src/main/java/com/example/promoda/helloworld/MainActivity.java
@@ -2,16 +2,39 @@ package com.example.promoda.helloworld;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Button hello_world_btn;
+    private TextView tv_hello_world;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hello_world_btn = (Button)findViewById(R.id.button_1);
+        tv_hello_world = (TextView)findViewById(R.id.tv_hello_world);
+
+
+        //create a method for button class..we re overriden the default on click method
+        hello_world_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_hello_world.setText("Hello World");
+
+
+            }
+        });
     }
 
     @Override
